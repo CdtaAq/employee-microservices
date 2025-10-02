ab9fe4540833adf84c97b604acc1b1a008b931c8
chris Taylor <ctay456a@gmail.com>
2025-10-02 19:12:07 -0400

    Create EmployeeServerApplication.java

---
+++ employee-server/src/main/java/com/example/server/EmployeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EmployeeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServerApplication.class, args);
+    }
+}
